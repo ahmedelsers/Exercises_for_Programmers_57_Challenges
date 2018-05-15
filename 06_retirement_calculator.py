10767bf4af2931dbd2ece0466fa1c518175079f8
Ahmed Elsers <ahmed.elsersi@gmail.com>
2018-05-15 15:26:54 +0300

    Update 06_retirement_calculator.py

--- 06_retirement_calculator.py
+++ 06_retirement_calculator.py
@@ -11,14 +11,14 @@ current_date = datetime.date.today()
 current_year = datetime.date.today().year
 
 years_to_retirement = retirement_age - current_age
-years_to_retirement_in_weeks = years_to_retirement * 48
+years_to_retirement_in_days = years_to_retirement * 365
 
-retirement_date = current_date + datetime.timedelta(weeks=years_to_retirement_in_weeks)
+retirement_date = current_date + datetime.timedelta(days=years_to_retirement_in_days)
 retirement_year = retirement_date.year
 
 
 if years_to_retirement < 0:
-    print("It's {}, You can retire now if you want!".format(current_year))
+    print("You could retire {} years ago if you want!".format(abs(years_to_retirement)))
 else:
-    print("You have {} years left until you can retire.\n It's {},"
-          "so you can retire in {}.".format(years_to_retirement,current_year,retirement_year))
+    print("You have {} years left until you can retire.\n"
+          "It's {}, so you can retire in {}.".format(years_to_retirement,current_year,retirement_year))

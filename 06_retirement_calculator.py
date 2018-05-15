8f7086490eff44833a642fc98e5997e4f248304c
Ahmed Elsers <ahmed.elsersi@gmail.com>
2018-05-15 13:29:26 +0300

    Create 06_retirement_calculator.py

---
+++ 06_retirement_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+import datetime
+
+
+current_age = int(input("What is your current age? "))
+retirement_age = int(input("At what age would you like to retire? "))
+
+current_date = datetime.date.today()
+current_year = datetime.date.today().year
+
+years_to_retirement = retirement_age - current_age
+years_to_retirement_in_weeks = years_to_retirement * 48
+
+retirement_date = current_date + datetime.timedelta(weeks=years_to_retirement_in_weeks)
+retirement_year = retirement_date.year
+
+
+if years_to_retirement < 0:
+    print("It's {}, You can retire now if you want!".format(current_year))
+else:
+    print("You have {} years left until you can retire.\n It's {},"
+          "so you can retire in {}.".format(years_to_retirement,current_year,retirement_year))

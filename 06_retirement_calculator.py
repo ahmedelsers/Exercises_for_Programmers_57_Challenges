92410d1078106777ba1407a57a3e3571a0f14f27
Ahmed Elsers <ahmed.elsersi@gmail.com>
2018-05-15 15:29:29 +0300

    Update 06_retirement_calculator.py

--- 06_retirement_calculator.py
+++ 06_retirement_calculator.py
@@ -3,22 +3,25 @@
 
 import datetime
 
+def main():
+    current_age = int(input("What is your current age? "))
+    retirement_age = int(input("At what age would you like to retire? "))
 
-current_age = int(input("What is your current age? "))
-retirement_age = int(input("At what age would you like to retire? "))
+    current_date = datetime.date.today()
+    current_year = datetime.date.today().year
 
-current_date = datetime.date.today()
-current_year = datetime.date.today().year
+    years_to_retirement = retirement_age - current_age
+    years_to_retirement_in_days = years_to_retirement * 365
 
-years_to_retirement = retirement_age - current_age
-years_to_retirement_in_days = years_to_retirement * 365
+    retirement_date = current_date + datetime.timedelta(days=years_to_retirement_in_days)
+    retirement_year = retirement_date.year
 
-retirement_date = current_date + datetime.timedelta(days=years_to_retirement_in_days)
-retirement_year = retirement_date.year
+    if years_to_retirement < 0:
+        print("You could retire {} years ago if you want!".format(abs(years_to_retirement)))
+    else:
+        print("You have {} years left until you can retire.\n"
+              "It's {}, so you can retire in {}.".format(years_to_retirement,current_year,retirement_year))
 
 
-if years_to_retirement < 0:
-    print("You could retire {} years ago if you want!".format(abs(years_to_retirement)))
-else:
-    print("You have {} years left until you can retire.\n"
-          "It's {}, so you can retire in {}.".format(years_to_retirement,current_year,retirement_year))
+if __name__ == "__main__":
+    main()

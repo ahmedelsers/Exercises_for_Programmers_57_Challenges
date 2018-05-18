f861ad4a44bda8459676b3b9f70d50733f280f58
Ahmed Elsers <ahmed.elsersi@gmail.com>
2018-05-18 21:58:30 +0300

    Update 07_rectangular_room_area.py

--- 07_rectangular_room_area.py
+++ 07_rectangular_room_area.py
@@ -16,30 +16,27 @@ def meter_to_feet(meter):
 
 def square_area(length, width, feet_or_meter):
     if feet_or_meter == 'f':
-        return round(feet_to_meter(length) * feet_to_meter(width), 3)
-    if feet_or_meter == 'm':
-        return round(meter_to_feet(length) * meter_to_feet(width), 3)
-
-
-try:
-    feet_or_meter = input("Your inputs will be in Feet or Meter? f or m: ")
-    if feet_or_meter == 'f':
-        length = float(input("What is the length of the room in feet? "))
-        width = float(input("What is the width of the room in feet? "))
         print("The area is:\n"
               "{} square feet\n"
-              "{} square meters".format((length*width),
-                                        square_area(length, width, feet_or_meter))
+              "{} square meters".format((length * width),
+                                        round(feet_to_meter(length) * feet_to_meter(width), 3))
               )
-    elif feet_or_meter == 'm':
-        length = float(input("What is the length of the room in meter? "))
-        width = float(input("What is the width of the room in meter? "))
+    if feet_or_meter == 'm':
         print("The area is:\n"
               "{} square meters\n"
               "{} square feet".format((length * width),
-                                      square_area(length, width, feet_or_meter))
+                                      round(meter_to_feet(length) * meter_to_feet(width), 3))
               )
-    else:
-        print("f for feet, m for meter, is it clear now?")
-except:
-    print("Please enter numbers only.")
+
+
+feet_or_meter = input("Your inputs will be in Feet or Meter? f or m: ")
+if feet_or_meter not in ['f', 'm']:
+    print("f for feet, m for meter, is it clear now?")
+    exit(0)
+else:
+    try:
+        length = float(input("What is the length of the room? "))
+        width = float(input("What is the width of the room? "))
+        square_area(length, width, feet_or_meter)
+    except:
+        print("Please enter numbers only.")
